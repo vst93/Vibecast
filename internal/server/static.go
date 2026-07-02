0f83d49fbbaec1466c934365f8ccb90878f3d384
v <vst93g@gmail.com>
2026-07-02 22:02:27 +0800

    feat: add MIME types for Office documents (doc/docx/xls/xlsx/ppt/pptx/odt/ods/odp/rtf)

--- internal/server/static.go
+++ internal/server/static.go
@@ -49,6 +49,17 @@ var extraMimeTypes = map[string]string{
 	// Other
 	".wasm":  "application/wasm",
 	".pdf":   "application/pdf",
+	// Office documents
+	".doc":   "application/msword",
+	".docx":  "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+	".xls":   "application/vnd.ms-excel",
+	".xlsx":  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+	".ppt":   "application/vnd.ms-powerpoint",
+	".pptx":  "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+	".odt":   "application/vnd.oasis.opendocument.text",
+	".ods":   "application/vnd.oasis.opendocument.spreadsheet",
+	".odp":   "application/vnd.oasis.opendocument.presentation",
+	".rtf":   "application/rtf",
 }
 
 // getContentType returns the MIME type for a file extension.
